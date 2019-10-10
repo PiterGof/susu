f4539eb9c1ebe03509dac4e6b12cc3ebfaec4b79
PiterGof <44323185+PiterGof@users.noreply.github.com>
2019-10-10 23:30:28 +0500

    Update lab.5.4.4.py

--- lab.5.4.4.py
+++ lab.5.4.4.py
@@ -1,18 +1,15 @@
 sum = int(input('Sum: '))
 
-def ruble(sum):
+def main(sum):
     rub = sum // 100
-
-def kopeck(sum)
     kop = sum % 100
-
-def main():
-
-    if rub = 1:
-        print(('%s рубль' % rub).upper())
-    elif rub / 5:
+    if rub % 10 in range(5, 10) or kop % 100 in range(10, 21) or kop % 10 == 0 and kop % 10 in range(5, 10) or kop % 100 in range(10, 21) or kop % 10 == 0:
         print(('%s рублей' % rub).upper())
-    else:
+        print(('%s копеек' % kop).upper())
+    elif rub % 10 in range(2, 5) and rub % 100 not in range(12, 15) and kop % 10 in range(2, 5) and kop % 100 not in range(12, 15):
         print(('%s рубля' % rub).upper())
-    print(('%s копейка' % cope).upper())
+        print(('%s копки' % kop).upper())
+    else:
+        print(('%s рубль' % rub).upper())
+        print(('%s копека' % kop).upper())
 main(sum)
