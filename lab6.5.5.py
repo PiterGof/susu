5834ae93833a1761fd8ab8296d9747d1dbc51a26
PiterGof <44323185+PiterGof@users.noreply.github.com>
2019-10-29 23:06:15 +0500

    Update lab6.5.5.py

--- lab6.5.5.py
+++ lab6.5.5.py
@@ -1,39 +1,40 @@
 import random
+import numpy as np
+
 
 f = open('input.txt', 'w')
 num_1 = random.randrange(2, 8)
-num_12 = random.randrange(2, 8)
-f.write('%d' % num_1 )
-f.write('%d' % num_12)
+num_2 = random.randrange(2, 8)
+num_3 = random.randrange(5, 16)
+f.write('%d \n' % num_1 )
+f.write('%d \n' % num_2)
+f.write('%d' % num_3)
 f.close()
 
 f = open('input.txt', 'r')
-lines = f.readline()
-n = lines[0]
-m = lines[1]
+lines = f.readlines()
+n = lines[0][0]
+m = lines[1][0]
+k = lines[2]
 n = int(n)
 m = int(m)
+k = int(k)
 f.close()
 
 matrix = [[random.randrange(10) for y in range(m)] for x in range(n)]
-matrix2 = [[random.randrange(10) for y in range(m)] for x in range(n)]
+matrix2 = [[random.randrange(10) for y in range(k)] for x in range(m)]
+
+matrix.sort()
+matrix2.sort()
 
 
-for i in range(n):
-    print(matrix[i])
-    print((matrix2[i][i]))
-    print('Hello')
+mat = np.array(matrix)
+mat2 = np.array(matrix2)
 
+total = mat.dot(mat2)
 
-for i in range(n):
-    print(matrix[i])
-
-print(' ')
-for i in range(n):
-    print(matrix2[i])
-
-# for i in matrix:
-#     for k in matrix2:
-#         for a in k:
-#             for j in i:
-#                 print(a*j)
+print(mat)
+print()
+print(mat2)
+print()
+print(total)
